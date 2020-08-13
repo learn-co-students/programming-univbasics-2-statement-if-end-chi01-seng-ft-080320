0e9717117574fedb0400431fb073b78f9bf4665a
John Easter <john@Johns-MacBook-Air.local>
2020-08-13 08:56:21 -0500

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,7 @@
 current_time = Time.now
-current_time = current_time.to_i
-if current_time % 2 == 0
+current_seconds = current_time.to_i
+if current_seconds % 2 == 0
   puts "Even!"
-else
+else 
   puts "Odd!"
 end
